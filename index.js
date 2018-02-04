db2b4926f14ae8814ed959bb0a24f8c68318e120
joshvito <josh.vito@gmail.com>
2018-02-04 18:00:43 -0500

    creates folders in mass before catergorization

--- index.js
+++ index.js
@@ -14,35 +14,57 @@ const collection = _dis.user().collection();
             "name": "All",
             "resource_url": "https://api.discogs.com/users/example/collection/folders/0"
             },
-            {
-            "id": 1,
-            "count": 20,
-            "name": "Uncategorized",
-            "resource_url": "https://api.discogs.com/users/example/collection/folders/1"
-            }
+            ...
         ]
     }
  */
 let user_folders  = _dis._request({
         url: `/users/${USER.name}/collection/folders`, method: 'GET', data: {}
     });
-let page = collection.getReleases(USER.name, 0, { page: 1, per_page: 1 });
+let page = collection.getReleases(USER.name, 0, { page: 1, per_page: 5 });
 
 let _FP = Promise.all([user_folders, page]);
 _FP.then((data) => {
     let [folders, page] = data;
     let user_folders_map = _foldersToMap(folders.folders)
-    let release = page.releases[0];
-    let folder = _discoverFolder(release, user_folders_map);
-    if (!folder.id) {
-        let newfolder = createUserFolder(folder.name)
-            .then(folder => {
-                user_folders_map = _foldersToMap([folder], _foldersToMap);
-                moveReleaseInstanceToFolder(release, folder);
-            })
-    }
+
+    let requiredFolders = page.releases.map(release => {
+        return _discoverFolder(release, user_folders_map);
+    })
+
+    const foldersToCreate = requiredFolders
+        .filter(folder => !folder.id) // we only want to create folders that don't exist
+        .filter((value, index, self) => {
+            // make sure we get unique folder names
+            return self.findIndex(o => o.name === value.name) === index;
+        });
+    
+    const folderPromises = foldersToCreate.map(f => createUserFolder(f.name));
+    const createdFolders = Promise.all(folderPromises);
+    createdFolders
+        .then(data => {
+            debugger;
+            return user_folders_map = _foldersToMap(data, user_folders_map);
+        })
+        .catch(error => {
+            debugger;
+        })
+    // let newFolder = categorizeRelease(release, folder); 
 })
 .catch(error => console.error(error));
+
+const categorizeRelease = (release, folder) => {
+    if (folder && !folder.id) {
+        createUserFolder(folder.name)
+            .then(data => {
+                moveReleaseInstanceToFolder(release, data);
+                return data;
+            })
+    } else {
+        moveReleaseInstanceToFolder(release, folder);
+        return folder
+    }
+}
 
 const createUserFolder = (name) => {
     return _dis._request({
@@ -56,68 +78,24 @@ const moveReleaseInstanceToFolder = (release, newFolder) => {
         url: `/users/${USER.name}/collection/folders/${release.folder_id}/releases/${release.id}/instances/${release.instance_id}`, method: 'POST', data: { folder_id: newFolder.id}
     }).then(response => console.log(response))
     .catch(error =>  {
-        debugger 
         console.error(error)
     });
 }
 
-
- /**
- * {pagination: Object, releases: Array(n)}
- *        pagination:Object {per_page: 1, items: 167, page: 1, …}
- *        releases: [
- * {
-      "id": 2464521,
-      "instance_id": 1,
-      "folder_id": 1,
-      "rating": 0,
-      "basic_information": {
-        "id": 2464521,
-        "title": "Information Chase",
-        "year": 2006,
-        "resource_url": "https://api.discogs.com/releases/2464521",
-        "thumb": "https://api-img.discogs.com/vzpYq4_kc52GZFs14c0SCJ0ZE84=/fit-in/150x150/filters:strip_icc():format(jpeg):mode_rgb()/discogs-images/R-2464521-1285519861.jpeg.jpg",
-        "cover_image": "https://api-img.discogs.com/vzpYq4_kc52GZFs14c0SCJ0ZE84/fit-in/500x500/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-2464521-1285519861.jpeg.jpg",
-        "formats": [
-          {
-            "qty": "1",
-            "descriptions": [ "Mini", "EP" ],
-            "name": "CDr"
-          }
-        ],
-        "labels": [
-          {
-            "resource_url": "https://api.discogs.com/labels/11647",
-            "entity_type": "",
-            "catno": "8BP059",
-            "id": 11647,
-            "name": "8bitpeoples"
-          }
-        ],
-        "artists": [
-          {
-            "id": 103906,
-            "name": "Bit Shifter",
-            "join": "",
-            "resource_url": "https://api.discogs.com/artists/103906",
-            "anv": "",
-            "tracks": "",
-            "role": ""
-          }
-        ]
-      },
-      "notes": [
-        {
-          "field_id": 3,
-          "value": "bleep bloop blorp."
-        }
+/**
+ * 
+ * @param {*} _release 
+ * @param {*} _foldersmap 
+ * @param {*} forceRecategorization boolean, optional, default = false
+ * // determine folder name
+    // if force or uncategorized
+    // check if it already exists
+    // return existing or new folder
  */
 const _discoverFolder = (_release, _foldersmap, forceRecategorization = false) => {
     const year = _release.basic_information.year;
     const name = _convertYearToFolderName(year);
-    // determine folder name
-    // check if it already exists
-    // return existing or new folder if force or uncategorized
+    
     if (forceRecategorization || _release.folder_id === 1) {
         if (_foldersmap && _foldersmap.hasOwnProperty(name)) {
             return _foldersmap[name]
@@ -127,6 +105,8 @@ const _discoverFolder = (_release, _foldersmap, forceRecategorization = false) =
                     name
                 }
         }
+    } else {
+        return _foldersmap[name]
     }
 }
 
