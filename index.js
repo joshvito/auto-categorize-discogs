018637342ec73fd7e31f588762dca7da1e657d45
joshvito <josh.vito@gmail.com>
2018-02-05 18:54:06 -0500

    refactor for organization and SOC

--- index.js
+++ index.js
@@ -1,86 +1,41 @@
-let Discogs = require('disconnect').Client;
-const USER = require('./pat.js');
 const FOLDERS = require('./src/folder');
+const API = require('./src/api');
+const PAGE_OPTIONS = { page: 1, per_page: 10 };
 
-const _dis = new Discogs({ userToken: USER.token }); // Authenticate by user token
-const collection = _dis.user().collection();
-/**
- * returns obj with user folders e.g.
-    {
-        "folders": [
-            {
-            "id": 0,
-            "count": 23,
-            "name": "All",
-            "resource_url": "https://api.discogs.com/users/example/collection/folders/0"
-            },
-            ...
-        ]
-    }
- */
-let user_folders  = _dis._request({
-        url: `/users/${USER.name}/collection/folders`, method: 'GET', data: {}
-    });
-let page = collection.getReleases(USER.name, 0, { page: 1, per_page: 10 });
-
-let _FP = Promise.all([user_folders, page]);
-_FP.then((data) => {
-    let [folders, page] = data;
-    let user_folders_map = FOLDERS.foldersToMap(folders.folders)
-
-    let requiredFolders = page.releases.map(release => {
-        return FOLDERS.discoverFolder(release, user_folders_map);
-    })
-
-    const foldersToCreate = FOLDERS.filterOutDupes(requiredFolders);
-    const folderPromises = foldersToCreate.map(f => createUserFolder(f.name));
+let categorizeByDecade = (user_folders_promise, page_promise) => {
     
-    if (folderPromises.length === 0 ) {
-        setTimeout(() => {
-            categorizeReleases(page.releases, user_folders_map);
-        }, 0)
-        return;
-    }
+    let _FP = Promise.all([user_folders_promise, page_promise]);
+    _FP.then((data) => {
+        let [folders, page] = data;
+        let user_folders_map = FOLDERS.foldersToMap(folders.folders)
     
-    user_folders_map = Promise.all(folderPromises).then(data => {
-        const updated_folders_map = FOLDERS.foldersToMap(data, user_folders_map); 
-        setTimeout(() => {
-            categorizeReleases(page.releases, updated_folders_map);
-        }, 0)
-        return updated_folders_map;
-    })
-    .catch(error => {
-        console.error(error);
-        return user_folders_map;
-    })
-})
-.catch(error => console.error(error));
-
-const categorizeReleases = (releases, foldermap) => {
-    releases.forEach(release => {
-        const folder = FOLDERS.discoverFolder(release, foldermap);
-        // short circuit if folder categories match
-        if (folder.id === release.folder_id) { 
-            console.warn(`${release.basic_information.title} is in the correct category.`)
-            return;
-         }
-        
-        moveReleaseInstanceToFolder(release, folder).then(data => {
-            console.log(`${release.basic_information.title} from ${release.basic_information.year} was re-categorized to ${folder.name}`)
-        }).catch(error => {
-            console.error(error, ` While trying to recategorize ${release.basic_information.title} from ${release.basic_information.year} to ${folder.name}`);
+        let requiredFolders = page.releases.map(release => {
+            return FOLDERS.discoverFolder(release, user_folders_map);
         })
-    });
+    
+        const foldersToCreate = FOLDERS.filterOutDupes(requiredFolders);
+        const folderPromises = foldersToCreate.map(f => API.createUserFolder(f.name));
+        
+        if (folderPromises.length === 0 ) {
+            setTimeout(() => {
+                API.categorizeReleases(page.releases, user_folders_map);
+            }, 0)
+            return;
+        }
+        
+        user_folders_map = Promise.all(folderPromises).then(data => {
+            const updated_folders_map = FOLDERS.foldersToMap(data, user_folders_map); 
+            setTimeout(() => {
+                API.categorizeReleases(page.releases, updated_folders_map);
+            }, 0)
+            return updated_folders_map;
+        })
+        .catch(error => {
+            console.error(error);
+            return user_folders_map;
+        })
+    })
+    .catch(error => console.error(error));
 }
 
-const createUserFolder = (name) => {
-    return _dis._request({
-        url: `/users/${USER.name}/collection/folders`, method: 'POST', data: { name }
-    });
-}
-
-const moveReleaseInstanceToFolder = (release, newFolder) => {
-    return _dis._request({
-        url: `/users/${USER.name}/collection/folders/${release.folder_id}/releases/${release.id}/instances/${release.instance_id}`, method: 'POST', data: { folder_id: newFolder.id}
-    });
-}
+categorizeByDecade(API.getUserFolders(), API.getPagePromise(PAGE_OPTIONS));
---
+++ src/api.js
@@ -0,0 +1,52 @@
+let Discogs = require('disconnect').Client;
+const USER = require('./pat.js');
+const FOLDERS = require('./folder');
+const _dis = new Discogs({ userToken: USER.token }); // Authenticate by user token
+const user_collection = _dis.user().collection();
+
+const categorizeReleases = (releases, foldermap) => {
+    releases.forEach(release => {
+        const folder = FOLDERS.discoverFolder(release, foldermap);
+        // short circuit if folder categories match
+        if (folder.id === release.folder_id) {
+            console.warn(`${release.basic_information.title} is in the correct category.`)
+            return;
+        }
+
+        moveReleaseInstanceToFolder(release, folder).then(data => {
+            console.log(`${release.basic_information.title} from ${release.basic_information.year} was re-categorized to ${folder.name}`)
+        }).catch(error => {
+            console.error(error, ` While trying to recategorize ${release.basic_information.title} from ${release.basic_information.year} to ${folder.name}`);
+        })
+    });
+}
+
+const createUserFolder = (name) => {
+    return _dis._request({
+        url: `/users/${USER.name}/collection/folders`, method: 'POST', data: { name }
+    });
+}
+
+const moveReleaseInstanceToFolder = (release, newFolder) => {
+    return _dis._request({
+        url: `/users/${USER.name}/collection/folders/${release.folder_id}/releases/${release.id}/instances/${release.instance_id}`, method: 'POST', data: { folder_id: newFolder.id }
+    });
+}
+
+const getUserFolders = () => { 
+    return _dis._request({
+        url: `/users/${USER.name}/collection/folders`, method: 'GET', data: {}
+    });
+}
+
+const getPagePromise = (options) => {
+    return user_collection.getReleases(USER.name, 0, options);
+}
+
+module.exports = {
+    categorizeReleases,
+    createUserFolder,
+    moveReleaseInstanceToFolder,
+    getUserFolders,
+    getPagePromise
+}
